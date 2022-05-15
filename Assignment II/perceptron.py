63474ea1803dea391af055363fad2bcf598f3530
Ankit Lamsal <lamsalankit@gmail.com>
2022-05-15 18:07:29 +0545

    Perceptron Class Created.

--- Assignment II/perceptron.py
+++ Assignment II/perceptron.py
@@ -24,3 +24,25 @@ def relu(x:ArrayLike)->ArrayLike:
         ArrayLike: numpy array with elements 0 or 1.
     """    
     return x * (x > 0)
+
+class Perceptron:
+    """Perceptron is the single unit used for neural network.
+    In this scenario, we are simulating training of Logical Gates (And, or,X-or, etc.). 
+    """    
+    def __init__(self, features_size=2, output_size=1, 
+               learning_rate=0.01, itreation=100, activation="sigmoid"):
+        """Constructor which takes hyperparameters for initialization.
+
+        Args:
+            features_size (int, optional): Row Size of the Feature Value(X). Defaults to 2.
+            output_size (int, optional): Row Size of the Label Value(y) Defaults to 1.
+            learning_rate (float, optional): Rate of Learning by the weights or biases (Ranges from 0 to 1). Defaults to 0.01.
+            itreation (int, optional): Number Itreation used for training. Defaults to 100.
+            activation (str, optional): Activation function to use for non linearity. Defaults to "sigmoid".
+        """        
+        self.learning_rate = learning_rate
+        self.features = features_size
+        self.output = output_size
+        self.itreation = itreation
+        self.weights, self.bias = self.initialize_params(self.features, self.output)
+        self.activation = activation
