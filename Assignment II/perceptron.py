e5ceffdb64424b757234ae8a853e06be4e5c2119
Ankit Lamsal <lamsalankit@gmail.com>
2022-05-15 18:06:38 +0545

    Relu function completed.

--- Assignment II/perceptron.py
+++ Assignment II/perceptron.py
@@ -13,3 +13,14 @@ def sigmoid(z:ArrayLike)-> ArrayLike:
          ArrayLike: Calculated value i.e. 1/(1 + e^(-x))
     """    
     return (1/(1+np.exp(-z)))
+
+def relu(x:ArrayLike)->ArrayLike:
+    """Returns 0 if x is less than 0 or equal to 0, else returns 1
+
+    Args:
+        x (ArrayLike): numpy array whose relu function is calculated.
+
+    Returns:
+        ArrayLike: numpy array with elements 0 or 1.
+    """    
+    return x * (x > 0)
